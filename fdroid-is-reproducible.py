45cbc4297db9193c9833537ceee0785dc575e93b
Felix C. Stegerman <flx@obfusk.net>
2021-07-28 18:15:01 +0200

    use index-v1.jar & CI

--- fdroid-is-reproducible.py
+++ fdroid-is-reproducible.py
@@ -18,70 +18,89 @@
 # requires: python3-click
 
 import json
-import os
 import sys
 import time
 import urllib.request
-import xml.etree.ElementTree as ET
+import zipfile
 
 from pathlib import Path
 
 import click
 
-INDEX_XML_URL = "https://f-droid.org/repo/index.xml"
+INDEX_JAR_URL = "https://f-droid.org/repo/index-v1.jar"
 VERIFIED_JSON_URL = "https://verification.f-droid.org/verified.json"
 DATADIR = Path.home() / ".cache" / "fdroid-is-reproducible"
-INDEX_XML = str(DATADIR / "index.xml")
-VERIFIED_JSON = str(DATADIR / "verified.json")
-METADATA_JSON = str(DATADIR / "metadata.json")
+INDEX_JAR = DATADIR / "index-v1.jar"
+INDEX_JSON = DATADIR / "index-v1.json"
+VERIFIED_JSON = DATADIR / "verified.json"
+METADATA_JSON = DATADIR / "metadata.json"
+BLACKLIST = set(["org.fdroid.fdroid.privileged.ota"])
 
 
+# FIXME: verify!
 def download_index(force_refresh=False):
-    if force_refresh or _outdated(INDEX_XML):
-        print("==> downloading index.xml...", file=sys.stderr)
-        with urllib.request.urlopen(INDEX_XML_URL) as fi:
-            with open(INDEX_XML, "wb") as fo:
+    if force_refresh or _outdated(INDEX_JSON):
+        print("==> downloading index-v1.jar...", file=sys.stderr)
+        with urllib.request.urlopen(INDEX_JAR_URL) as fi:
+            with INDEX_JAR.open("wb") as fo:
                 fo.write(fi.read())
+        with zipfile.ZipFile(INDEX_JAR) as zf:
+            with zf.open(INDEX_JSON.name) as fi:
+                with INDEX_JSON.open("wb") as fo:
+                    fo.write(fi.read())
+        INDEX_JAR.unlink()
     if force_refresh or _outdated(METADATA_JSON):
-        print("==> parsing index.xml...", file=sys.stderr)
+        print("==> parsing index-v1.json...", file=sys.stderr)
         apps = {}
-        with open(INDEX_XML) as fh:
-            for e in ET.parse(fh).getroot():
-                if e.tag != "application":
+        with INDEX_JSON.open() as fh:
+            data = json.load(fh)
+            for app in data["apps"]:
+                appid = app["packageName"]
+                if appid in BLACKLIST:
                     continue
-                appid = e.find("id").text.strip()
-                name = e.find("name").text.strip()
-                version = e.find("marketversion").text.strip()
-                vercode = int(e.find("marketvercode").text.strip())
-                apps[appid] = dict(name=name, version=version, vercode=vercode)
-        with open(METADATA_JSON, "w") as fh:
-            json.dump(apps, fh)
+                name = (app.get("name") or app["localized"]["en-US"]["name"]).strip()
+                version = app["suggestedVersionName"]
+                vercode = int(app["suggestedVersionCode"])
+                signed_by_developer = 0
+                for pkg in data["packages"][appid]:
+                    assert pkg["packageName"] == appid
+                    if pkg["versionCode"] == vercode:
+                        if pkg.get("srcname"):
+                            signed_by_developer |= 1    # could be binaries
+                        else:
+                            signed_by_developer |= 2    # signatures
+                assert signed_by_developer in (0, 1, 3)
+                apps[appid] = dict(name=name, version=version, vercode=vercode,
+                                   signed_by_developer=signed_by_developer)
+        with METADATA_JSON.open("w") as fh:
+            json.dump(apps, fh, sort_keys=True, indent=2)
 
 
 def download_verified(force_refresh=False):
     if force_refresh or _outdated(VERIFIED_JSON):
         print("==> downloading verified.json...", file=sys.stderr)
         with urllib.request.urlopen(VERIFIED_JSON_URL) as fi:
-            with open(VERIFIED_JSON, "wb") as fo:
+            with VERIFIED_JSON.open("wb") as fo:
                 fo.write(fi.read())
 
 
 def load_metadata():
-    with open(METADATA_JSON) as fh:
+    with METADATA_JSON.open() as fh:
         return json.load(fh)
 
 
 def load_verified():
-    with open(VERIFIED_JSON) as fh:
+    with VERIFIED_JSON.open() as fh:
         return json.load(fh)
 
 
-def _outdated(file):
-    if not os.path.exists(file):
-        return True
-    if time.time() - os.stat(file).st_mtime > 24*60*60:
-        return True
-    return False
+def _outdated(path):
+    return not path.exists() or time.time() - path.stat().st_mtime > 24 * 60 * 60
+
+
+# FIXME
+def _fmt_signed_by_developer(value):
+    return {0: "missing", 1: "unknown", 3: "both"}[value]
 
 
 # FIXME
@@ -110,10 +129,12 @@ def cli(search, force_refresh, query):
             total += 1
             if vercode > last[0]:
                 last = (vercode, app["local"]["versionName"])
+        sbd = _fmt_signed_by_developer(data["signed_by_developer"])
         print(appid + ":")
         print("  name:", data["name"])
         print("  current version:", data["version"])
         print("  current version code:", data["vercode"])
+        print("  signed by developer:", sbd)
         print("  status:", status)
         print("  total verified:", total)
         if last[1]:
